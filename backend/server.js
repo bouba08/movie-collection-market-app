3216b07059c087d1906931b7c8afed4f0b55b2b7
JoshuaBrophy <josh666brophy@hotmail.co.uk>
2024-03-19 09:42:43 +0000

    first commit + server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
-
-// Define routes
-app.get('/', (req, res) => {
-  res.send('Hello from the backend!');
-});
-
-// Start the server
 const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/api/movies', require('./api/movieRoutes'));
+
+// Start server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
