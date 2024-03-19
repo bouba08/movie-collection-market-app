0eec9bb1c2e81b4e10e13ceb1135981fcce06e84
JoshuaBrophy <josh666brophy@hotmail.co.uk>
2024-03-19 10:18:29 +0000

    created and populated Movie.js
    inside backend controllers

---
+++ backend/controllers/movieController.js
@@ -0,0 +1,18 @@
+const Movie = require('./models/Movie');
+
+//Get movies
+const getAllMovies = async (req, res) => {
+  try {
+    const movies = await Movie.find();
+    res.json(movies);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+
+
+module.exports = {
+    getAllMovies,
+}
