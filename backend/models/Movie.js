694b0ec424281bf0994e72d89ea9e65cd16a5577
brayner11 <braynerlopez11@hotmail.com>
2024-03-19 12:22:14 +0000

    models/movie.js done added fields

--- backend/models/Movie.js
+++ backend/models/Movie.js
@@ -5,7 +5,52 @@ const movieSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  
+  director: {
+    type: String,
+    required: true
+  },
+  year: {
+    type: Number,
+    required: true
+  },
+  genre: {
+    type: String,
+    required: true
+  },
+  plot: {
+    type: String,
+    required: true
+  },
+  cast: [{
+    type: String,
+    required: true
+  }],
+  rating: {
+    type: Number,
+    default: 0
+  },
+  duration: {
+    type: Number,
+    required: true
+  },
+  releaseDate: {
+    type: Date,
+    required: true
+  },
+  language: {
+    type: String,
+    required: true
+  },
+  country: {
+    type: String,
+    required: true
+  },
+  awards: {
+    type: [String]
+  },
+  boxOffice: {
+    type: Number
+  },
 });
 
 module.exports = mongoose.model('Movie', movieSchema);
