bb42f0c7005c24cc5dccf5738e01050cf42e89bd
JoshuaBrophy <josh666brophy@hotmail.co.uk>
2024-03-22 15:59:39 +0000

    able to add comonents
    after a day of developing them and running into compiling errors ive mannaged to add more requested data feilds in the movie details.js and movie card.js i have also add improved css for a uniform movie card to details card so when they are side by side there in proportion to each other

--- frontend/app/src/MovieDetail.js
+++ frontend/app/src/MovieDetail.js
@@ -19,11 +19,22 @@ const MovieDetail = ({ movie, onAddToCollection, onRemoveFromCollection }) => {
     <div className="movie-detail-container">
       <div className="movie-detail">
         <h2>{movie.Title}</h2>
-        <p><strong>Release Year:</strong> {movie.Year}</p>
         <p><strong>Genre:</strong> {movie.Genre}</p>
-        <p><strong>Rating:</strong> {movie.imdbRating}</p>
         <p><strong>Plot:</strong> {movie.Plot}</p>
         <p><strong>Box Office:</strong> {movie.BoxOffice}</p>
+        <p><strong>Awards:</strong> {movie.Awards}</p>
+        <p><strong>Age Rating:</strong> {movie.Rated}</p>
+        <p><strong>Director(s):</strong> {movie.Director}</p>
+        <p><strong>Writer(s):</strong> {movie.Writer}</p>
+        <p><strong>Language:</strong> {movie.Language}</p>
+        <p><strong>Runtime:</strong> {movie.Runtime}</p>
+        <p><strong>Country:</strong> {movie.Country}</p>
+        <p><strong>Released Date:</strong> {movie.Released}</p>
+        {movie.Ratings && movie.Ratings.map((rating, index) => (
+          <p key={index}><strong>{rating.Source} Rating:</strong> {rating.Value}</p>
+        ))}
+        <p><strong>Cast:</strong> {movie.Actors}</p>
+        
         <label>
           <input
             type="checkbox"
